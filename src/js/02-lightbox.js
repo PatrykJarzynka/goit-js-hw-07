a04463e55ce733429f277fe31cb597676c2fabee
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-11-14 20:16:25 +0100

    Zaczęto tworzenie zadania 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+let array = [];
+galleryItems.forEach(element => {
+  //tworzenie linku
+  const link = document.createElement('a');
+  link.classList.add('gallery__item');
+  link.href = element.original;
+  //tworzenie obrazka i łączenie z linkiem
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = element.preview;
+  img.alt = element.description;
+  link.append(img);
+  //wrzucenie całości do tablicy
+  array.push(link);
+});
+
+gallery.append(...array);
+
+
+
+//console.log(galleryItems);
